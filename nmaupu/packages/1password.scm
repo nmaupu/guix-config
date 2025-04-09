bffaab0e3bb07a3e00b2dedf20b8976b16b92325
Nicolas Maupu <nmaupu@gmail.com>
2025-04-09 11:43:32 +0200

    Added comment on the 1password package

--- nmaupu/packages/1password.scm
+++ nmaupu/packages/1password.scm
@@ -5,6 +5,19 @@
   #:use-module (gnu packages compression)
   #:use-module ((nonguix licenses) #:prefix license:))
 
+;; This package requires some rights to be changed on "op" binary (as per the readme)
+;; - A group onepassword-cli has to be created
+;; - To be used, the user should be added to this group
+;; - op binary has to be chown to root:onepassword-cli
+;; - op binary has to be chmod g+s
+;; Something along those lines:
+;;
+;; (simple-service 'onepassword-setgid-helper privileged-program-service-type
+;;                 (list (privileged-program
+;;                         (program  (file-append 1password-cli "/bin/op"))
+;;                         (group "onepassword-cli")
+;;                         (setgid? #t))))
+
 (define-public 1password-cli
   (package
     (name "1password-cli")
