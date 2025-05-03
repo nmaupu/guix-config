3a5b94a78ef7d456e0731ce9b0fb47c59b1bef3a
Nicolas Maupu <nmaupu@gmail.com>
2025-05-03 16:57:35 +0200

    Updating polybar-themes

--- nmaupu/packages/polybar-themes.scm
+++ nmaupu/packages/polybar-themes.scm
@@ -1,13 +1,13 @@
 (define-module (nmaupu packages polybar-themes)
   #:use-module (guix build-system trivial)
-  #:use-module (guix licenses)
+  #:use-module ((guix licenses) #:prefix licenses:)
   #:use-module (guix packages)
   #:use-module (guix git-download))
 
 (define-public polybar-themes
   (package
     (name "polybar-themes")
-    (version "b8bd8c5fb3a70df7fabbb3500d8021f784743e01")
+    (version "bcc9957b569841c05c950d41d49e07e2edc17e47")
     (source
      (origin
        (method git-fetch)
@@ -16,7 +16,7 @@
               (commit version)))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "05q6yc553zpigi68xjv5hjly3b8a499n443j5l24nphkzjvjp3qq"))))
+        (base32 "10kb35rjfadjgimrly9gnmfxylmqinj34v39hcc4qcd6ya0q1hwv"))))
     (build-system trivial-build-system)
     (arguments
      `(#:modules ((guix build utils))
@@ -26,7 +26,7 @@
                           (source (assoc-ref %build-inputs "source")))
                      (mkdir-p out)
                      (copy-recursively (string-append source "/simple") out)))))
-    (home-page "")
-    (synopsis "")
-    (description "")
-    (license expat)))
+    (home-page "https://github.com/nmaupu/polybar-themes.git")
+    (synopsis "A huge collection of polybar themes with different styles, colors and variants.")
+    (description "A huge collection of polybar themes with different styles, colors and variants.")
+    (license licenses:gpl3)))
