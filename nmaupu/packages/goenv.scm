18ff9944d228c2b26a6b43cddb8ba91201ecfc98
Nicolas Maupu <nmaupu@gmail.com>
2025-06-12 11:29:54 +0200

    Updating goenv

--- nmaupu/packages/goenv.scm
+++ nmaupu/packages/goenv.scm
@@ -7,7 +7,7 @@
 (define-public goenv
   (package
     (name "goenv")
-    (version "2.2.22")
+    (version "2.2.24")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -16,7 +16,7 @@
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "0i18aqngmbhpvmwg0951qmkpy6k28lr64z7wwib3r1b7jkpdzlvh"))))
+                "18yya53rkpr4mcagvghvn50kd8dng4hgjy3d3j82qjm9mr5k42ks"))))
     (build-system copy-build-system)
     (arguments
      `(#:phases
