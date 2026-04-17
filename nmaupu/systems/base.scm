111cd87afd59c5db55a98f57e292ad51afa086c2
Nicolas Maupu <nmaupu@gmail.com>
2026-04-17 12:36:31 +0200

    Switch back to nonguix subt. after config ipv6

--- nmaupu/systems/base.scm
+++ nmaupu/systems/base.scm
@@ -140,8 +140,8 @@
                                            guix-service-type
                                            (guix-extension
                                             (substitute-urls
-                                             ;; (append (list "https://substitutes.nonguix.org")
-                                             (append (list "https://nonguix-proxy.ditigal.xyz")
+                                             (append (list "https://substitutes.nonguix.org") ;; ipv6 only
+                                             ;; (append (list "https://nonguix-proxy.ditigal.xyz")
                                                      %default-substitute-urls))
                                             (authorized-keys
                                              (append (list (plain-file "nonguix.pub"
